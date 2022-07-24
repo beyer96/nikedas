26b12020fa5543ae8216095a61a5eacd37ddf441
Lukas Beyer <lukas.beyer@juicymo.cz>
2022-07-24 19:41:08 +0200

    Add dropzone.js for drag & drop images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Vendor
 import "@hotwired/turbo-rails";
 import "controllers";
 import "trix";
 import "@rails/actiontext";
 import "choices.js";
+
+// Custom
+import "./custom_dropzone";
